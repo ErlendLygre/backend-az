03bfafb7e4804c1a6f20e790a642848816e2d5dc
Erlend Tangeraas Lygre <erly@equinor.com>
2022-05-24 18:16:28 +0200

    all the things

---
+++ --no-restore/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace __no_restore;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
